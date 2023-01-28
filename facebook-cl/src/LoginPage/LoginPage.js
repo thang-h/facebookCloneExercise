10ff5481c512db98983adcb8b5aa56e05edfb9ea
Kansvict <hoangthang260321@gmail.com>
2023-01-27 20:13:08 -0800

    Initial commit

---
+++ facebook-cl/src/LoginPage/LoginPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./LoginPage.css";
+
+function LoginPage() {
+    return (
+    <div className='Login'>
+        <div className='facebook'>
+            <div className='facebooktext'>
+                Facebook
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default LoginPage
